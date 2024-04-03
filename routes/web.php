39aa70f742469151a20645209fff7ede76fc2447
Gustavo Gualda <104951600+iCrowleySHR@users.noreply.github.com>
2024-04-03 18:06:34 -0300

    13 - Grupo de Rotas

--- routes/web.php
+++ routes/web.php
@@ -12,10 +12,10 @@ Route::get('/', function () {
 });
 
 // Acessa url empresa pelo meio get
-// Route::get('/empresa', function(){
-//     // Retorna o arquivo empresa.blade.php da resources/views/site/empresa
-//     return view('site/empresa');
-// });
+Route::get('/firma', function(){
+    // Retorna o arquivo empresa.blade.php da resources/views/site/empresa
+    return view('site/firma');
+});
 
 // ============================================================================================================
 
@@ -54,10 +54,69 @@ Route::view('/empresa' , 'site/empresa');
 Route::get('/news', function(){
     return view('news');
 })->name('noticias');
-// Nomeamos a rota news para noticias
+// Nomeamos a rota news com o nome noticias
 
 Route::get('/novidades', function(){
     // Na /novidades redirectiona para a /news usando seu Nome (noticias)
     return redirect()->route('noticias');
 });
 // A rota pode mudar, porem se o nome continuar o redirecionamento não falhará
+
+// ============================================================================================================
+
+// GRUPO DE ROTAS
+
+// PREFIX
+// Usando PREFIX agora tudo que está nessa função precisa ter admin/
+// EX: admin/dashboard
+Route::prefix('admin')->group(function(){
+    Route::get('dashboard', function(){
+        return 'ADMIN/DASHBOARD';
+    });
+    Route::get('users', function(){
+        return 'ADMIN/USERS';
+    });
+    Route::get('clientes', function(){
+        return 'ADMIN/CLIENTES';
+    });
+});
+
+// NAMEGROUP
+// Agora automaticamente todo os nomes começaram com noadmin.
+Route::name('noadmin.')->group(function(){
+    Route::get('noadmin/dashboard' , function(){
+        return 'noadmin/dashboard <br>' . 'Name é: ' . route('noadmin.users');
+    })->name('dashboard');
+
+    Route::get('noadmin/users' , function(){
+        return 'noadmin/users <br>' . 'Name é: ' . route('noadmin.users');
+    })->name('users');
+
+    Route::get('noadmin/clientes' , function(){
+        return 'noadmin/clientes <br>' . 'Name é: ' . route('noadmin.users');
+    })->name('clientes');
+});
+
+// NAMEGROUP COM PREFIX
+Route::group([
+    'prefix' => 'dono', // PREFIX ANTES DA ROTA VAI TER DONO/{E A ROTA}
+    'as' => 'dono.'     // ANTES DO NAME ESCOLHIDO VAI TER DONO.{E O NOME ESCOLHIDO}
+    // EX: http://localhost/cursoLaravel/public/dono/clientes
+], function(){  
+    Route::get('/dashboard',function(){
+        return 'DASHBOARD DO DONO';
+    })->name('dashboard');
+
+    Route::get('/users', function(){
+        return 'USUARIOS DO DONO';
+    })->name('users');
+
+    Route::get('/clientes', function(){
+        return 'CLIENTES DO DONO';
+    })->name('clientes');
+});
+
+Route::get('/dono', function(){
+    return redirect()->route('dono.dashboard');
+});
+
